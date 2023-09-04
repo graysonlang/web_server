d55fe9da8bc4b9f171d6dba832ce51848ed5e19e
Grayson Lang <grayson.lang@gmail.com>
2023-09-04 12:49:51 -0700

    Clean up.

--- start_web_server.sh
+++ start_web_server.sh
@@ -7,13 +7,17 @@ set -euo pipefail
 export SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)
 source "${SCRIPT_DIR}/web_server.sh"
 
+abspath() {
+  echo $(cd "${1}"; pwd -P)
+}
+
 # Check if the server is not running.
 if [[ -z "${WEB_SERVER_PID}" ]]; then
   DIR=${@:-.}
   OUTPUT_TO_FILE=">~/Desktop/http_server_log.txt 2>&1"
   OUTPUT_TO_NULL='>/dev/null 2>&1'
   # Start localhost Python web server in the background.
-  command="nohup python3 -m ${WEB_SERVER_CMD} -d \"${DIR}\" ${OUTPUT_TO_NULL} &"
+  command="nohup python3 -m ${WEB_SERVER_CMD} -d \"$(abspath ${DIR})\" ${OUTPUT_TO_NULL} &"
   eval ${command}
   echo "Started web server (${WEB_SERVER_CMD}) with pid: $(get_server_pid)."
 else
